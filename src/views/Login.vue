4620e627a36b1c56dbd5c16d304eb3a781ae6bdc
SagarIdeen <isagarideen@gmail.com>
2023-03-28 16:09:36 +0530

    [Update] validation form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,38 @@
 <script setup>
-import { ref, computed, onMounted } from 'vue';
+import { ref, reactive, onMounted, computed } from 'vue';
 import { useRouter } from 'vue-router'
 import axios from 'axios'
 import jwt_decode from "jwt-decode";
+import { useVuelidate } from '@vuelidate/core'
+import { required, email } from '@vuelidate/validators'
+import { useToast } from "primevue/usetoast";
 
 
 const router = useRouter()
-const email = ref('');
-const password = ref('');
-
-const name = ref('')
 const isRegister = ref(false);
+const toast = useToast();
+
+
+
+const showError = () => {
+    toast.add({ severity: 'error', summary: 'Error', detail: 'Invalid Credentials...!!', life: 3000 });
+};
+
+const formData = reactive({
+    email: "",
+    name: "",
+    password: ""
+})
+
+const rules = computed(() => {
+    return {
+        email: { required, email },
+        name: { required },
+        password: { required }
+    }
+})
+
+const v$ = useVuelidate(rules, formData)
 
 onMounted(() => {
     const token = localStorage.getItem("token_user")
@@ -21,57 +43,66 @@ onMounted(() => {
 })
 
 const onToggle = () => {
+    // formData.email = "";
+    formData.name = "";
+    formData.password = "";
     isRegister.value = !isRegister.value
 }
 
 async function onSubmit() {
     console.log('is toggle falg :', isRegister.value);
     if (isRegister.value) {
-        try {
-            const response = await axios.post("user", {
-                "name": name.value,
-                "email": email.value,
-                "password": password.value,
-                "role": "USER"
-            })
-            console.log('create user data', response.data);
+        const result = await v$.value.$validate();
+        if (result) {
+            try {
+                await axios.post("user", {
+                    "name": formData.name,
+                    "email": formData.email,
+                    "password": formData.password,
+                    "role": "USER"
+                })
+                    .then((response) => {
+                        console.log('Register response :', response.data);
+                        isRegister.value = !isRegister.value
+                    })
 
-            name.value = ''
-            // email.value = ''
-            password.value = ''
-
-            isRegister.value = !isRegister.value
-
-        } catch (error) {
-            console.log('errrrr', error)
+            } catch (error) {
+                console.log('errrrr', error)
+            }
         }
     } else if (!isRegister.value) {
+        formData.name = "default"
+        const result = await v$.value.$validate();
+        if (result) {
 
-        try {
-            const response = await axios.post("auth/login", {
-                "email": email.value,
-                "password": password.value
-            })
-            // console.log('response data', response.data);
+            try {
+                await axios.post("auth/login", {
+                    "email": formData.email,
+                    "password": formData.password
+                }).then((response) => {
+                    const accessToken = response.data.access_token
 
-            const accessToken = response.data.access_token
-
-
-
-
-            axios.defaults.headers.common["Authorization"] =
-                "Bearer " + accessToken;
-            await localStorage.setItem("token_user", accessToken);
+                    axios.defaults.headers.common["Authorization"] =
+                        "Bearer " + accessToken;
+                    localStorage.setItem("token_user", accessToken);
 
 
-            var decoded = jwt_decode(response.data.access_token);
-            console.log('decoded token :' + JSON.stringify(decoded))
-            await localStorage.setItem("user_id", decoded.sub)
+                    var decoded = jwt_decode(response.data.access_token);
+                    console.log('decoded token :' + JSON.stringify(decoded))
+                    localStorage.setItem("user_id", decoded.sub)
+                })
+                    .finally(() => {
+                        router.push('/')
+                    })
 
-            router.push('/')
-        } catch (error) {
-            console.log('errrrr', error)
+            } catch (error) {
+                console.log('errrrr', JSON.stringify(error))
+                showError()
+            }
+
+
         }
+
     }
 
 }
@@ -93,14 +124,18 @@ async function onSubmit() {
 
                     <div>
                         <label for="email1" class="block text-900 text-xl font-medium mb-2">Email</label>
-                        <InputText id="email1" type="text" placeholder="Email address" class="w-full md:w-30rem mb-5"
-                            style="padding: 1rem" v-model="email" />
+                        <InputText id="email1" type="text" placeholder="Email address" class="w-full md:w-30rem"
+                            style="padding: 1rem" v-model="formData.email" /><br />
+                        <span v-for="error in v$.email.$errors" :key="error.$uid" class="text-red-500">{{ error.$message
+                        }}</span>
 
-                        <label for="password1" class="block text-900 font-medium text-xl mb-2">Password</label>
-                        <Password id="password1" v-model="password" placeholder="Password" :toggleMask="true"
-                            class="w-full mb-5" inputClass="w-full" inputStyle="padding:1rem"></Password>
+                        <label for="password1" class="block text-900 font-medium text-xl mt-5 mb-2">Password</label>
+                        <Password id="password1" v-model="formData.password" placeholder="Password" :toggleMask="true"
+                            class="w-full" inputClass="w-full" inputStyle="padding:1rem"></Password><br />
+                        <span v-for="error in v$.password.$errors" :key="error.$uid" class="text-red-500">{{ error.$message
+                        }}</span>
 
-                        <Button label="Sign In" class="w-full p-3 text-xl" @click="onSubmit"></Button>
+                        <Button label="Sign In" class="w-full p-3 text-xl mt-5" @click="onSubmit"></Button>
                         <div class="flex align-items-center justify-content-between mt-5 gap-5">
                             <span></span>
                             <Button link class="font-medium no-underline ml-2  cursor-pointer" @click="onToggle"
@@ -121,18 +156,23 @@ async function onSubmit() {
 
                     <div>
                         <label for="name1" class="block text-900 text-xl font-medium mb-2">Name</label>
-                        <InputText id="name1" type="text" placeholder="Name address" class="w-full md:w-30rem mb-5"
-                            style="padding: 1rem" v-model="name" />
+                        <InputText id="name1" type="text" placeholder="Name" class="w-full md:w-30rem" style="padding: 1rem"
+                            v-model="formData.name" /><br />
+                        <span v-for="error in v$.name.$errors" :key="error.$uid" class="text-red-500">{{ error.$message
+                        }}</span>
+                        <label for="email1" class="block text-900 text-xl font-medium mt-5 mb-2">Email</label>
+                        <InputText id="email1" type="text" placeholder="Email address" class="w-full md:w-30rem "
+                            style="padding: 1rem" v-model="formData.email" /><br />
+                        <span v-for="error in v$.email.$errors" :key="error.$uid" class="text-red-500">{{ error.$message
+                        }}</span>
 
-                        <label for="email1" class="block text-900 text-xl font-medium mb-2">Email</label>
-                        <InputText id="email1" type="text" placeholder="Email address" class="w-full md:w-30rem mb-5"
-                            style="padding: 1rem" v-model="email" />
+                        <label for="password1" class="block text-900 font-medium text-xl mt-5 mb-2">Password</label>
+                        <Password id="password1" v-model="formData.password" placeholder="Create a Password" class="w-full "
+                            inputClass="w-full" inputStyle="padding:1rem"></Password><br />
+                        <span v-for="error in v$.password.$errors" :key="error.$uid" class="text-red-500">{{ error.$message
+                        }}</span>
 
-                        <label for="password1" class="block text-900 font-medium text-xl mb-2">Password</label>
-                        <Password id="password1" v-model="password" placeholder="Create a Password" class="w-full mb-5"
-                            inputClass="w-full" inputStyle="padding:1rem"></Password>
-
-                        <Button label="Register" class="w-full p-3 text-xl" @click="onSubmit"></Button>
+                        <Button label="Register" class="w-full p-3 text-xl mt-5" @click="onSubmit"></Button>
                         <div class="flex align-items-center justify-content-between mt-5 gap-5">
                             <span></span>
                             <Button link class="font-medium no-underline ml-2  cursor-pointer" @click="onToggle"
@@ -143,6 +183,7 @@ async function onSubmit() {
                 </div>
             </div>
         </div>
+        <Toast />
     </div>
 </template>
 
